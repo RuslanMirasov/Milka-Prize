186f0fcf35da05398e0e328cd060525f337662cc
Ruslan <info@mirasov.dev>
2026-06-25 13:37:24 +0200

    add header

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,6 @@
 import { hidePreloader, initRandomPresent, initDateTimer } from './helpers.js';
 
-const START_DATE = '01.08.2026 00:00'; // Дата и время начала акции
+const START_DATE = '01.01.2026 00:00'; // Дата и время начала акции
 const FINISH_DATE = '30.09.2050 23:59'; // Дата и время окончания акции
 const PRESENTS_FORMAT = 'jpg'; // Формат изображений подарков
 
